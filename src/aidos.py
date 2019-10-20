77e2b4cf9de95d1d520bef29446a9d0355466f7a
Lazlo <glebite@gmail.com>
2019-10-20 08:23:25 -0400

    Added in logging and playing about

--- src/aidos.py
+++ src/aidos.py
@@ -1,10 +1,28 @@
 # aidos.py
+import os
+import logging
 
+LOG_FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 
 class Aidos():
-    def __init__(self):
-        pass
+    def __init__(self, run_name):
+        self.run_name = run_name
+        self.setup_logger()
 
+    def setup_logger(self):
+        logging.basicConfig(format=LOG_FORMAT, level=os.environ.get("LOGLEVEL", "DEBUG"))
+        self.log = logging.getLogger(self.run_name)
+
+    def declare_test(self, test_title):
+        self.log.info("TEST_DESCRIPTION: {}".format(test_title))
+
+    def test_step(self, step_description):
+        self.log.info("STEP: {}".format(step_description))
+
+    def declare_result(self, test_result):
+        self.log.info("RESULTS: {}".format(test_result))
+
+                      
 def main():
     """
     main
--- test/test_aidos.py
+++ test/test_aidos.py
@@ -9,5 +9,19 @@ from aidos import Aidos
 
 @pytest.mark.test_id(1)
 def test_aidos_creation():
-    aidos_obj = Aidos()
+    aidos_obj = Aidos("test")
     assert aidos_obj is not None
+
+@pytest.mark.test_id(2)
+def test_aidos_logging_creation():
+    aidos_obj = Aidos("test")
+    assert aidos_obj.log is not None
+
+@pytest.mark.test_id(3)
+def test_aidos_no_run_name():
+    try:
+        aidos_obj = Aidos()
+        assert False
+    except TypeError as raised_exception:
+        assert True
+        
